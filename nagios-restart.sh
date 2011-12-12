e30c2c4c0d365d7b443a0ccf4522344486706835
Francois POIROTTE <francois.poirotte@c-s.fr>
2011-12-12 16:16:17 +0100

    Signal SIGTERM/SIGKILL pour Nagios (#926).
    Dans le script chargé d'envoyer un signal SIGTERM journalier à Nagios,
    on fait attention de ne pas tuer accidentellement le script de
    redémarrage par la même veine (ce qui empêcherait le relancement de
    Nagios à la fin du script).
    
    De la même manière, on fait bien attention à ne pas tuer trop de
    processus (via SIGKILL) dans le script d'arrêt de Nagios de VigiConf.
    
    Change-Id: I00044b3c24cb121997f6bedc9f8de9ee18b8ad3b
    Refs: #926.
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/440
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Aurelien BOMPARD <aurelien.bompard@c-s.fr>

--- nagios-restart.sh
+++ nagios-restart.sh
@@ -5,8 +5,40 @@
 #  * http://tracker.nagios.org/view.php?id=71
 #  * http://tracker.nagios.org/view.php?id=92
 
+# On tente de déterminer l'emplacement du fichier de PID de Nagios
+# à partir des emplacements les plus probables pour le fichier nagios.cfg.
+nagios_pidfile=
+for d in /etc/nagios3 /etc/nagios; do
+    if test -f "$d/nagios.cfg"; then
+        possible_pidfile=`egrep "^[[:space:]]*lock_file" "$d/nagios.cfg"|cut -d= -f2`
+        # Élimine les espaces éventuels autour du chemin.
+        possible_pidfile=`echo $possible_pidfile`
+        if test -f $possible_pidfile; then
+            nagios_pidfile=$possible_pidfile
+            break
+        fi
+    fi
+done
+
+# Pas de fichier de PID, on ne fait rien
+# (Nagios s'est probablement arrêté correctement entre temps).
+if test -z "$nagios_pidfile"; then
+    exit 0
+fi
+nagios_pid=`head -n 1 $nagios_pidfile`
+
+# On ne fait rien si Nagios n'est pas en cours d'exécution.
+if ! ps -p $nagios_pid > /dev/null 2>&1; then
+    exit 0
+fi
+
 /etc/init.d/nagios stop
 sleep 2
-pkill nagios
+
+# On envoie un signal SIGTERM au processus principal de Nagios
+# ainsi qu'à tous ses sous-processus dans le cas où le "stop" classique
+# n'aurait pas suffit.
+kill -- -$nagios_pid 2>/dev/null
 sleep 2
+
 /etc/init.d/nagios start
