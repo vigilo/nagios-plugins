1ab661651d2d4116dc8f97d162eae978bf8cb663
Francois POIROTTE <francois.poirotte@c-s.fr>
2015-01-09 18:28:24 +0100

    Mise à jour des copyrights pour 2015.
    Change-Id: I9284fbfa807afc51b301e47d93938da57372c338
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1556
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- nagios-restart.sh
+++ nagios-restart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (C) 2011-2014 CS-SI
+# Copyright (C) 2011-2015 CS-SI
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 # Redémarrage régulier de Nagios pour éviter les fuites de mémoire de l'ePN.
