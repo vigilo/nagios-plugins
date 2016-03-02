3ded3508ffd480b563d9a09f6a809e9c62c51806
Francois POIROTTE <francois.poirotte@c-s.fr>
2016-03-02 10:20:19 +0100

    Mise à jour des copyrights
    Change-Id: I17e1858cdcb2281cadc204102baaa2096a40380f
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1958
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- plugins/utils_vigilo.py
+++ plugins/utils_vigilo.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # vim: fileencoding=utf-8 sw=4 ts=4 et ai
-# Copyright (C) 2015-2015 CS-SI
+# Copyright (C) 2015-2016 CS-SI
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 import sys
